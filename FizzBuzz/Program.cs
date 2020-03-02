b0dbc5c89f375af4242e702ac375f2a1f145fe74
Morgan Hay <morganhay.94@gmail.com>
2020-03-02 13:47:23 -0600

    Fizz Buzz Exercise

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountFizzBuzz();
+            //Fizz appears 267 times
+            //Buzz appears 134 times
+            //FizzBuzz appears 67 times
+        }
+
+        static void CountFizzBuzz()
+        {
+            int Fizz = 0;
+            int Buzz = 0;
+            int FizzBuzz = 0;
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0){
+                    System.Console.WriteLine("FizzBuzz");
+                    FizzBuzz++;
+                }
+                else if(i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                    Fizz++;
+                }
+                else if(i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                    Buzz++;
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+            System.Console.WriteLine("Fizz appeared " + Fizz + " times!");
+            System.Console.WriteLine("Buzz appeared " + Buzz + " times!");
+            System.Console.WriteLine("Fizz Buzz appeared " + FizzBuzz + " times!");
+        }
+    }
+}
